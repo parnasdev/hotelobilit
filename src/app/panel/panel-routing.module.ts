bf86a08db4104748ca60afc6a611b4313244aecd
m.mohammadi <m.mohamadi490@gmail.com>
2023-04-26 12:24:09 +0430

    add some files to project and separate style files

--- src/app/panel/panel-routing.module.ts
+++ src/app/panel/panel-routing.module.ts
@@ -1,53 +1,112 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanelComponent } from './panel/panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-    // {
-  //   path: 'transfer',
-  //   // canActivate: [PanelItemGuardService],
-  //   // data: {permitions: ['Transfer']},
-  //   loadChildren: () => import('../transfer/transfer.module').then(m => m.TransferModule)
-  // },
-  // {
-  //   path: 'transferRate',
-  //   // canActivate: [PanelItemGuardService],
-  //   // data: {permitions: ['Transfer']},
-  //   loadChildren: () => import('../transfer-rate/transfer-rate.module').then(m => m.TransferRateModule)
-  // },
-  // {
-  //   path: 'cities',
-  //   // canActivate: [PanelItemGuardService],
-  //   // data: {permitions: ['Cities']},
-  //   loadChildren: () => import('../cities/cities.module').then(m => m.CitiesModule)
-  // },
-  // {
-  //   path: 'hotel',
-  //   // canActivate: [PanelItemGuardService],
-  //   // data: {permitions: ['Hotel']},
-  //   loadChildren: () => import('../hotel/panel/hotel-panel.module').then(m => m.HotelPanelModule)
-  // },
-  // {
-  //   path: 'file-manager',
-  //   loadChildren: () => import('../file-manager/file-manager.module').then(m => m.FileManagerModule)
-  // },
-  // {
-  //   path: 'user',
-  //   // data: {permitions: ['User','User.list']},
-  //   // canActivate: [PanelItemGuardService],
-  //   loadChildren: () => import('../user/user.module').then(m => m.UserModule)
-  // }, 
-  // {
-  //   path: 'setting',
-  //   // canActivate: [PanelItemGuardService],
-  //   // data: {permitions: ['Setting.change']},
-  //   loadChildren: () => import('../setting/setting.module').then(m => m.SettingModule)
-  // },
-  // {
-  //   path: 'roomType',
-  //   // canActivate: [PanelItemGuardService],
-  //   // data: {permitions: ['RoomType']},
-  //   loadChildren: () => import('../room-type/room-type.module').then(m => m.RoomTypeModule)
-  // },
+  {
+    path: '',
+    component: PanelComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+      // {
+      //   path: 'tour',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Tour']},
+      //   loadChildren: () => import('../tour/panel/tour-panel.module').then(m => m.TourPanelModule)
+      // },
+      // {
+      //   path: 'transfer',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Transfer']},
+      //   loadChildren: () => import('../transfer/transfer.module').then(m => m.TransferModule)
+      // },
+      // {
+      //   path: 'transferRate',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Transfer']},
+      //   loadChildren: () => import('../transfer-rate/transfer-rate.module').then(m => m.TransferRateModule)
+      // },
+      // {
+      //   path: 'financial',
+      //   loadChildren: () => import('../financial/financial.module').then(m => m.FinancialModule)
+      // },
+      // {
+      //   path: 'agencies',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Agency']},
+      //   loadChildren: () => import('../agencies/agencies.module').then(m => m.AgenciesModule)
+      // },
+      // {
+      //   path: 'profile',
+      //   component: EditComponent
+      // },
+      // {
+      //   path: 'requestReserves',
+      //   component: RequestReserveComponent
+      // },
+      // {
+      //   path: 'cities',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Cities']},
+      //   loadChildren: () => import('../cities/cities.module').then(m => m.CitiesModule)
+      // },
+      // {
+      //   path: 'hotel',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Hotel']},
+      //   loadChildren: () => import('../hotel/panel/hotel-panel.module').then(m => m.HotelPanelModule)
+      // },
+      // {
+      //   path: 'file-manager',
+      //   loadChildren: () => import('../file-manager/file-manager.module').then(m => m.FileManagerModule)
+      // },
+      // {
+      //   path: 'user-agency',
+      //   loadChildren: () => import('../user/user-agencies/user-agencies.module').then(m => m.UserAgenciesModule)
+      // },
+      // {
+      //   path: 'user',
+      //   // data: {permitions: ['User','User.list']},
+      //   // canActivate: [PanelItemGuardService],
+      //   loadChildren: () => import('../user/user.module').then(m => m.UserModule)
+      // }, {
+      //   path: 'redirector',
+      //   loadChildren: () => import('../redirector/redirector.module').then(m => m.RedirectorModule)
+      // },
+      // // {
+      // //   path: 'blog',
+      // //   // canActivate: [PanelItemGuardService],
+      // //   // data: {permitions: ['Post']},
+
+      // //   loadChildren: () => import('../blog/panel/blog-panel.module').then(m => m.BlogPanelModule)
+      // // },
+      // {
+      //   path: 'setting',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['Setting.change']},
+      //   loadChildren: () => import('../setting/setting.module').then(m => m.SettingModule)
+      // },
+      // // {
+      // //   path: 'gallery',
+      // //   loadChildren: () => import('../gallery/gallery.module').then(m => m.GalleryModule)
+      // // },
+      // {
+      //   path: 'roomType',
+      //   // canActivate: [PanelItemGuardService],
+      //   // data: {permitions: ['RoomType']},
+      //   loadChildren: () => import('../room-type/room-type.module').then(m => m.RoomTypeModule)
+      // },
+      // {
+      //   path: 'comment',
+      //   loadChildren: () => import('../comment/comment.module').then(m => m.CommentModule)
+      // },
+
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/panel/panel.module.ts
+++ src/app/panel/panel.module.ts
@@ -5,10 +5,16 @@ import { PanelRoutingModule } from './panel-routing.module';
 import { PanelComponent } from './panel/panel.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [PanelComponent, SidebarComponent, HeaderComponent],
+  declarations: [
+    PanelComponent,
+    SidebarComponent,
+    HeaderComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     PanelRoutingModule
