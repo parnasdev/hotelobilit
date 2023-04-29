b4d54f62e9b78ce08ade6258595201c818acf90b
milad <miladrealkord@gmail.com>
2023-04-29 17:59:05 +0430

    panel dark mode

--- src/app/panel/panel/panel.component.ts
+++ src/app/panel/panel/panel.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import {SettingService} from "../../Core/Services/setting.service";
+
 declare let $: any;
 
 @Component({
@@ -9,6 +10,7 @@ declare let $: any;
   styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent implements OnInit {
+  isDarkMode = false;
 
   constructor(public title: Title,
               public setting: SettingService) {
@@ -16,13 +18,31 @@ export class PanelComponent implements OnInit {
 
   ngOnInit(): void {
     $('.toggle-sidebar').click(() => {
-      $('.sidebar').animate({
+      $('.p-sidebar').animate({
         width: ['toggle', 'swing'],
         opacity: ['toggle', 'swing'],
       })
       $('.main').toggleClass('main-full').animate(1500)
+      $('.toggle-sidebar-open').css('display', 'flex')
+      $('.toggle-sidebar-open').addClass('animated fadeInRight')
+
     })
+    $('.toggle-sidebar-open').click(() => {
+      $('.p-sidebar').animate({
+        width: ['toggle', 'swing'],
+        opacity: ['toggle', 'swing'],
+      })
+      $('.main').removeClass('main-full').animate(1500)
+      $('.toggle-sidebar-open').css('display', 'none')
+    })
+    $('.dark-mode').click(() => {
+      $('.s-panel').toggleClass('dark-mode-panel');
+    })
+
     this.title.setTitle('پنل کاربری هتل و بلیط')
   }
 
+  darkMode():any {
+    this.isDarkMode=!this.isDarkMode
+  }
 }
