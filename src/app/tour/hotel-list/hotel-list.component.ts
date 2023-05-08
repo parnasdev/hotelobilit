cd7a8e925b1346a255f733c74237979e28578475
milad <miladrealkord@gmail.com>
2023-05-08 14:32:07 +0430

    add transfer-rate

--- src/app/tour/hotel-list/hotel-list.component.ts
+++ src/app/tour/hotel-list/hotel-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ResponsiveService} from "../../Core/Services/responsive.service";
 
 @Component({
   selector: 'prs-hotel-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./hotel-list.component.scss']
 })
 export class HotelListComponent {
+  isMobile = false;
+  isDesktop = false;
+  isTablet = false;
+  constructor(
+    public mobileService: ResponsiveService,
+  ) {
+    this.isMobile = mobileService.isMobile()
+    this.isDesktop = mobileService.isDesktop()
 
+    this.isTablet = mobileService.isTablet()
+  }
 }
