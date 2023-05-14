3cabcb9a79165a15e431f0ac714e8bc155e0ae32
iman hp <iman.hp.dev5294@gmail.com>
2023-05-14 09:15:36 +0330

    hotel clean

--- src/app/hotel/hotel-routing.module.ts
+++ src/app/hotel/hotel-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ListComponent
-  },
 ];
 
 @NgModule({
--- src/app/hotel/hotel.module.ts
+++ src/app/hotel/hotel.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HotelRoutingModule } from './hotel-routing.module';
-import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
-    ListComponent
   ],
   imports: [
     CommonModule,
