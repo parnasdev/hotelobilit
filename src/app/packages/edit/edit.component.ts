d494db2c46f0c97274e6d0f2ce3fedefd26564f5
m.mohammadi <m.mohamadi490@gmail.com>
2023-08-07 12:30:04 +0330

    some debug rooms

--- src/app/packages/edit/edit.component.ts
+++ src/app/packages/edit/edit.component.ts
@@ -81,6 +81,7 @@ export class EditComponent extends AddComponent implements OnInit {
 
 
   convertPackages() {
+    this.packages = [];
     this.tourData.packages.forEach((x: any) => {
       let item: PackageTourDTO = {
         hotel_id: x.hotel.id,
