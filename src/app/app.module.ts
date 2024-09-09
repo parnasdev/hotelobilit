7c742722c158771c0da169fd17bce3bd2763d804
Pouya-A <58390637+Pouya-A@users.noreply.github.com>
2024-09-09 12:44:45 +0330

    najva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import {CommonProjectModule} from "./common-project/common-project.module";
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { SwPush } from '@angular/service-worker';
+
 // import { ServiceWorkerModule } from '@angular/service-worker';
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -35,12 +38,13 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
             defaultLanguage: 'en'
         }),
         CommonProjectModule,
-        // ServiceWorkerModule.register('ngsw-worker.js', {
-        //   enabled: !isDevMode(),
-        //   // Register the ServiceWorker as soon as the application is stable
-        //   // or after 30 seconds (whichever comes first).
-        //   registrationStrategy: 'registerWhenStable:30000'
-        // })
+
+        ServiceWorkerModule.register('custome-service-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
 
     ],
   providers: [authInterceptorProviders],
