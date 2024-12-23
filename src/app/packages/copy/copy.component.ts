679a2048d3dcea156183102342d94640a8f9dfdd
Pouya-A <58390637+Pouya-A@users.noreply.github.com>
2024-12-23 14:33:24 +0330

    export reserve-info.component.html as pdf

--- src/app/packages/copy/copy.component.ts
+++ src/app/packages/copy/copy.component.ts
@@ -36,22 +36,22 @@ export class CopyComponent extends EditComponent implements OnInit {
   }
   override submit(): void {
     this.setReq()
-    // console.log(this.req,this.flights)
-    this.tourApi.createTour(this.req).subscribe((res: any) => {
-      if (res.isDone) {
-        this.message.showMessageBig(res.message);
-        this.errorService.clear();
-        this.router.navigateByUrl('/panel/packages');
-      }
-    }, (error: any) => {
-      if (error.status == 422) {
-        this.errorService.recordError(error.error.data);
-        this.message.showMessageBig('اطلاعات ارسال شده را مجددا بررسی کنید')
-      } else {
-        this.message.showMessageBig('مشکلی رخ داده است لطفا مجددا تلاش کنید')
-      }
-      this.checkError.check(error);
-    })
+    console.log(this.req,this.flights)
+    // this.tourApi.createTour(this.req).subscribe((res: any) => {
+    //   if (res.isDone) {
+    //     this.message.showMessageBig(res.message);
+    //     this.errorService.clear();
+    //     this.router.navigateByUrl('/panel/packages');
+    //   }
+    // }, (error: any) => {
+    //   if (error.status == 422) {
+    //     this.errorService.recordError(error.error.data);
+    //     this.message.showMessageBig('اطلاعات ارسال شده را مجددا بررسی کنید')
+    //   } else {
+    //     this.message.showMessageBig('مشکلی رخ داده است لطفا مجددا تلاش کنید')
+    //   }
+    //   this.checkError.check(error);
+    // })
   }
 
 
