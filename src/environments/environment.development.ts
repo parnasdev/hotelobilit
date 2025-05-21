0caae98e9d27e38ee0b3b29537db6ec7123cc997
Pouya-A <58390637+Pouya-A@users.noreply.github.com>
2025-05-21 11:03:08 +0330

    changes on group cahnge popup sync_Price

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
-    BACK_END_IP: 'https://api.hotelobilit.com/api/v1/',
+    BACK_END_IP: 'http://api.hotelobilit.com/api/v1/',
 
-    BACK_END_IP_V2: 'https://api.hotelobilit.com/api/v2/',
-  BACK_END_IP_V3: 'https://api.hotelobilit.com/api/v3/',
+    BACK_END_IP_V2: 'http://api.hotelobilit.com/api/v2/',
+  BACK_END_IP_V3: 'http://api.hotelobilit.com/api/v3/',
 
   fileManagerAddress: '',
-    BACK_END_UPLOAD: 'https://api.hotelobilit.com/api/',
-    PANEL_URL: 'https://api.hotelobilit.com/',
+    BACK_END_UPLOAD: 'http://api.hotelobilit.com/api/',
+    PANEL_URL: 'http://api.hotelobilit.com/',
     SITE_URL: 'tour.hotelobilit.com',
     TWIN_ROOM_ID: 148,
     LOCAL_URL: 'hotelobilit-sandbox.iran.liara.run',
